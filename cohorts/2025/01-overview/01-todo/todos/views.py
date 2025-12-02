888b52432ce627a11926b1e0737cea0990374b73
Your Name <you@example.com>
2025-12-02 00:48:50 -0800

    Q4: Todo logic - views

--- cohorts/2025/01-overview/01-todo/todos/views.py
+++ cohorts/2025/01-overview/01-todo/todos/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Todo
 
-# Create your views here.
+# List all TODOs
+class TodoListView(ListView):
+    model = Todo
+    template_name = 'home.html'
+    context_object_name = 'todos'
+
+# Create a new TODO
+class TodoCreateView(CreateView):
+    model = Todo
+    template_name = 'todo_form.html'
+    fields = ['title', 'description', 'due_date']
+    success_url = reverse_lazy('todo_list')
+
+# Update an existing TODO
+class TodoUpdateView(UpdateView):
+    model = Todo
+    template_name = 'todo_form.html'
+    fields = ['title', 'description', 'due_date', 'is_completed']
+    success_url = reverse_lazy('todo_list')
+
+# Delete a TODO
+class TodoDeleteView(DeleteView):
+    model = Todo
+    template_name = 'todo_confirm_delete.html'
+    success_url = reverse_lazy('todo_list')
+
+# Toggle TODO completion status
+def toggle_complete(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    todo.is_completed = not todo.is_completed
+    todo.save()
+    return redirect('todo_list')
